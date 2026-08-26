391a3f65224fa879261699c0744e2e5b6272e7a6
Thomas Stemler <stemlertho@gmail.com>
2026-08-26 12:40:55 -0500

    Added response status codes for better api parsing on the client

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 
 from database_client import DatabaseClient
@@ -48,56 +48,68 @@ Routes we need.
 """
 
 @app.get("/api/messages", response_model=MessagesGet)
-async def get_messages():
+async def get_messages(response: Response):
     messages = db_client.get_messages()
+    response.status_code = 200
     return {"contents": messages}
 
 @app.post("/api/messages")
-async def create_message(message: Message):
+async def create_message(message: Message, response: Response):
     db_client.put_message(message.model_dump())
+    response.status_code = 201
     return {"status": "Message created"}
 
 @app.get("/api/users", response_model=UsersGet)
-async def get_users():
+async def get_users(response: Response):
     users = db_client.get_users()
+    response.status_code = 200
     return {"contents": users}
 
 @app.get("/api/users/{user_id}", response_model=User)
-async def get_user(user_id: int):
+async def get_user(user_id: int, response: Response):
     user = db_client.get_user(user_id)
     if user is None:
+        response.status_code = 404
         return {"error": "User not found"}
+    response.status_code = 200
     return User(id=user["id"], username=user["username"], color=user["color"])
 
 @app.get("/api/auth/{username}", response_model=User)
-async def authenticate_user(username: str):
+async def authenticate_user(username: str, response: Response):
     user = db_client.get_user_by_username(username)
     if user is None:
+        response.status_code = 404
         return {"error": "User not found"}
 
     if user["logged_in"]:
+        response.status_code = 400
         return {"error": "User already logged in"}
 
     db_client.login_user(user["id"])  # Log the user in
     
+    response.status_code = 200
     return User(id=user["id"], username=user["username"], color=user["color"])
 
 @app.post("/api/logout/{username}")
-async def logout_user(username: str):
+async def logout_user(username: str, response: Response):
     user = db_client.get_user_by_username(username)
     if user is None:
+        response.status_code = 404
         return {"error": "User not found"}
 
     if not user["logged_in"]:
+        response.status_code = 400
         return {"error": "User is not logged in"}
 
     db_client.logout_user(user["id"])  # Log the user out
     
+    response.status_code = 200
     return {"status": "User logged out"}
 
 @app.post("/api/users")
-async def create_user(user: User):
+async def create_user(user: User, response: Response):
     db_client.put_user(user.model_dump())
+    response.status_code = 201
     return {"status": "User created"}
 
 @app.websocket("/ws")
