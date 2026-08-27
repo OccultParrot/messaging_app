d657c9077fda5217bdf1cc6f6b054a61f3e0dce6
Thomas Stemler <stemlertho@gmail.com>
2026-08-27 10:11:31 -0500

    Finished message sending function

--- server/connection_manager.py
+++ server/connection_manager.py
@@ -2,13 +2,19 @@ from fastapi import WebSocket
 from data_types import User
 
 class ConnectionManager:
-    connections: dict[int, WebSocket]
+    connections: dict[int, WebSocket] = {}
 
     async def connect(self, user: User, websocket: WebSocket):
         if user.id == None:
             return
         
         for c in self.connections.values():
-            await c.send_json({"joined": user})
+            await c.send_json({"joined": {"id": user.id, "username": user.username, "color": user.color}})
 
         self.connections[user.id] = websocket
+
+    async def send_message(self, context: dict, user_id: int):
+        for k in self.connections.keys():
+            if k == user_id:
+                continue # Skip sender
+            await self.connections[k].send_json(context)
